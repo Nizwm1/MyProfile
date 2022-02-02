5f54e70b4d496c87c4bcd179c798357229eb72e3
Nizwm1 <87936739+Nizwm1@users.noreply.github.com>
2022-02-02 18:13:06 +0530

    Create new.js

---
+++ new.js
@@ -0,0 +1,6 @@
+function myFunction() {
+    document.getElementById("greet").innerHTML = "Welcome to my page!";
+    document.getElementById("greet").style.color = "rgb(172, 221, 236)"
+    document.getElementById("greet").style.fontSize = "30px"
+    document.getElementById("greet").style.fontStyle = "italic"
+}
